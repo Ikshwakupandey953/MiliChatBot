b69bb81c761f739f0951842094dfa73247341591
Ikshwakupandey953 <98660511+Ikshwakupandey953@users.noreply.github.com>
2022-04-22 15:31:54 +0530

    Update luna.py

--- luna.py
+++ luna.py
@@ -53,7 +53,7 @@ async def type_and_send(message):
 @luna.on_message(filters.command("repo") & ~filters.edited)
 async def repo(_, message):
     await message.reply_text(
-        "[GitHub](https://github.com/thehamkercat/LunaChatBot)"
+        "[GitHub](citels.tech)"
         + " | [Group](t.me/citels)",
         disable_web_page_preview=True,
     )
